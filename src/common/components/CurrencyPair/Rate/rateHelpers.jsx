6502f672b6af06b99ad12fc34ab1eb02e3ce33ef
Vladimir <olvnikon@gmail.com>
2018-08-03 22:05:57 +0200

    Refactor rateHelpers function to more declarative code.

--- src/common/components/CurrencyPair/Rate/rateHelpers.jsx
+++ src/common/components/CurrencyPair/Rate/rateHelpers.jsx
@@ -1,23 +1,36 @@
 // @flow
 import React from 'react';
+import { compose } from 'redux';
 import styles from './Rate.scss';
 
-const mapRightSide = chunks => (
-  chunks.map((chunk, index) => (
-    <span
-      key={index} // eslint-disable-line react/no-array-index-key
-      className={styles[`chunk-${index + 1}`]}
-    >
-      {chunk}
-    </span>
-  )));
+const mapChunk = (chunk, index) => (
+  <span
+    key={index} // eslint-disable-line react/no-array-index-key
+    className={styles[`chunk-${index + 1}`]}
+  >
+    {chunk}
+  </span>
+);
+const mapChunks = chunks => chunks.map(mapChunk);
+const splitByChunks = value => value.match(/.{1,2}/g) || ['00'];
+
+const mapRightSide = compose(
+  mapChunks,
+  splitByChunks,
+);
+
+const mapLeftSide = leftSide => <span>{`${leftSide}.`}</span>;
+
+const splitRateLeftRight = rate => rate.toString().split('.');
 
 // ToDo: refactor from imperative to declarative
 // eslint-disable-next-line import/prefer-default-export
 export const mapRate = (rate: number) => {
-  const [leftSide, rightSide] = rate.toString().split('.');
-  const leftLayout = <span>{`${leftSide}.`}</span>;
-  const chunks = rightSide.match(/.{1,2}/g) || ['00'];
-  const rightLayout = mapRightSide(chunks);
-  return <span>{leftLayout}{rightLayout}</span>;
+  const [leftSide, rightSide] = splitRateLeftRight(rate);
+  return (
+    <span>
+      {mapLeftSide(leftSide)}
+      {mapRightSide(rightSide)}
+    </span>
+  );
 };
